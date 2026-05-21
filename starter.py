8cb95a2c755432dda838af456e34604da5688429
Gaurav-Sharma-007 <myspacegaurav7@gmail.com>
2026-05-21 19:04:23 +0530

    First pushh

---
+++ starter.py
@@ -0,0 +1,39 @@
+import asyncio
+from llama_index.core.agent.workflow import FunctionAgent
+import os
+from openai import AzureOpenAI
+
+
+
+subscription_key = os.getenv("AZURE_OPENAI_KEY")
+
+client = AzureOpenAI(
+    api_version="2024-12-01-preview",
+    azure_endpoint="https://gaurav-testing.openai.azure.com/openai/v1/chat/completions",
+    api_key=subscription_key,
+)
+
+
+# Define a simple calculator tool
+def multiply(a: float, b: float) -> float:
+    """Useful for multiplying two numbers."""
+    return a * b
+
+
+# Create an agent workflow with our calculator tool
+agent = FunctionAgent(
+    tools=[multiply],
+    llm=client,
+    system_prompt="You are a helpful assistant that can multiply two numbers.",
+)
+
+
+async def main():
+    # Run the agent
+    response = await agent.run("What is 1234 * 4567?")
+    print(str(response))
+
+
+# Run the agent
+if __name__ == "__main__":
+    asyncio.run(main())
